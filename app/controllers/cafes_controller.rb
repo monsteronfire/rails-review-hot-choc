0e3b20c40626426e57c69b961f576afeda271142
monsteronfire <monsteronfire@gmail.com>
2016-02-05 14:42:34 +0800

    adds CRUD methods to reviews and cafes controller

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -2,4 +2,21 @@ class CafesController < ApplicationController
   def search
     render json: Cafe.all.select(:lat, :lng, :name).to_json
   end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
 end
